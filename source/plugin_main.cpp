e5617ef9cf36f3ee2f0d435645867672c17f4381
geomoded-sdk <geomoded.sdk@gmail.com>
2026-04-12 16:37:02 -0300

    Fix C linkage for Lilu plugin

--- source/plugin_main.cpp
+++ source/plugin_main.cpp
@@ -21,26 +21,28 @@ static const char* disableArg[] = { "-ffacloff", "-noffacl" };
 static const char* debugArg[] = { "ffacl_debug", "ffacl=0" };
 static const char* betaArg[] = { "-ffaclbeta" };
 
-void pluginStart() {
-    auto* plugin = ForceACLPlugin::getInstance();
-    if (plugin) {
-        IOLog("ForceACL v%s - Initializing...\n", PLUGIN_VERSION);
-        plugin->init();
-        plugin->start();
+extern "C" {
+    void forceACLStart() {
+        auto* plugin = ForceACLPlugin::getInstance();
+        if (plugin) {
+            IOLog("ForceACL v" xStringify(MODULE_VERSION) " - Initializing...\n");
+            plugin->init();
+            plugin->start();
+        }
     }
-}
 
-extern "C" PluginConfiguration ADDPR(config) = {
-    xStringify(PRODUCT_NAME),
-    parseModuleVersion(xStringify(MODULE_VERSION)),
-    LiluAPI::AllowNormal,
-    disableArg,
-    2,
-    debugArg,
-    2,
-    betaArg,
-    1,
-    KernelVersion::HighSierra,
-    KernelVersion::Sonoma,
-    pluginStart
-};
+    PluginConfiguration ADDPR(config) = {
+        xStringify(PRODUCT_NAME),
+        parseModuleVersion(xStringify(MODULE_VERSION)),
+        LiluAPI::AllowNormal,
+        disableArg,
+        2,
+        debugArg,
+        2,
+        betaArg,
+        1,
+        KernelVersion::HighSierra,
+        KernelVersion::Sonoma,
+        forceACLStart
+    };
+}
