31bb62d2a83f1e9b77bed77e968297068aafd14a
geomoded-sdk <geomoded.sdk@gmail.com>
2026-04-12 14:58:24 -0300

    Remove Lilu dependency for now

--- source/plugin_main.cpp
+++ source/plugin_main.cpp
@@ -1,31 +1,18 @@
 /**
  * ForceACL Plugin Entry Point
- * Lilu plugin bootstrap and initialization
+ * Standalone (no Lilu dependency for now)
  */
 
 #include <mach/mach_time.h>
 #include <IOKit/IOService.h>
 #include <IOKit/IOLib.h>
 
-#include "plugin_start.hpp"
-#include "kern_version.hpp"
-#include "kern_util.hpp"
-
 #include "ForceACL/ForceACL.hpp"
 
-// Global verbose and debug flags
-bool gForceACLVerbose = false;
-bool gForceACLDebug = false;
-
-extern "C" {
-int PE_parse_boot_arg_num(const char* arg, void* value);
-}
-
-// Simple plugin entry
 void pluginStart() {
     auto* plugin = ForceACLPlugin::getInstance();
     if (plugin) {
-        IOLog("ForceACL v%s - Initializing...\n", PLUGIN_VERSION);
+        IOLog("ForceACL v1.0.0 - Initializing...\n");
         plugin->init();
         plugin->start();
     }
