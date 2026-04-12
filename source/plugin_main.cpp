8e527dbaf3fd31b2ffb196b5186ad3d41f4a3d0b
geomoded-sdk <geomoded.sdk@gmail.com>
2026-04-12 14:46:53 -0300

    Simplify plugin entry for Lilu 1.7.x

--- source/plugin_main.cpp
+++ source/plugin_main.cpp
@@ -21,36 +21,8 @@ extern "C" {
 int PE_parse_boot_arg_num(const char* arg, void* value);
 }
 
-// Disable arguments
-static const char* disableArg[] = {
-    "-ffacloff",
-    "-noffacl"
-};
-
-// Debug arguments
-static const char* debugArg[] = {
-    "ffacl_debug",
-    "ffacl=0"
-};
-
-// Beta arguments  
-static const char* betaArg[] = {
-    "-ffaclbeta"
-};
-
-// Plugin start function
-extern "C" void pluginStart() {
-    // Check for verbose boot
-    if (PE_parse_boot_arg_num("v", nullptr)) {
-        gForceACLVerbose = true;
-    }
-    
-    // Check for debug mode
-    if (PE_parse_boot_arg_num("ffacl_debug", nullptr)) {
-        gForceACLDebug = true;
-    }
-    
-    // Initialize plugin
+// Simple plugin entry
+void pluginStart() {
     auto* plugin = ForceACLPlugin::getInstance();
     if (plugin) {
         IOLog("ForceACL v%s - Initializing...\n", PLUGIN_VERSION);
@@ -58,19 +30,3 @@ extern "C" void pluginStart() {
         plugin->start();
     }
 }
-
-// Plugin configuration for Lilu 1.7.x
-extern "C" PluginConfiguration ADDPR(config) = {
-    xStringify(PRODUCT_NAME),
-    parseModuleVersion(xStringify(MODULE_VERSION)),
-    LiluAPI::AllowNormal,
-    disableArg,
-    2,
-    debugArg,
-    2,
-    betaArg,
-    1,
-    KernelVersion::HighSierra,
-    KernelVersion::Ventura,
-    pluginStart
-};
